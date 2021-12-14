72c55d126fc509bf1c55edca2163813e3e6f01f2
Shijun <986244259@qq.com>
2021-12-14 16:28:13 +0800

    111

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webContent = "Hello World!"
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+	log.Fatal(http.ListenAndServe(":80", nil))
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, webContent)
+}
